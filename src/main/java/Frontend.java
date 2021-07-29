a1066931265996b68ee3d9038fc579e19c2f6584
Alex Shishkin <gitbox@inbox.ru>
2021-07-29 16:18:22 +0300

    second

---
+++ src/main/java/Frontend.java
@@ -0,0 +1,16 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Frontend extends HttpServlet {
+    private String login = "";
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{  }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {  }
+
+}
